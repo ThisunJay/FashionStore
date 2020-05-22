2aedcb9de4ce635d264514ea290fd3b03edcd0e7
ThisunJay <jayamaduthisun@gmail.com>
2020-05-22 23:43:43 +0530

    REST API class

--- DataAccessLayer/ClassAPI.cs
+++ DataAccessLayer/ClassAPI.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +11,33 @@ namespace DataAccessLayer
     public class ClassAPI
     {
         private const string URL = "https://jsonplaceholder.typicode.com/posts";
-        private string urlParameters = "?api_key=123";
+        //private string urlParameters = "?api_key=123";
 
+        private static void CreateObject()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.ContentLength = DATA.Length;
+            StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+            //requestWriter.Write(DATA);
+            requestWriter.Close();
+
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                Stream webStream = webResponse.GetResponseStream();
+                StreamReader responseReader = new StreamReader(webStream);
+                string response = responseReader.ReadToEnd();
+                Console.Out.WriteLine(response);
+                responseReader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("-----------------");
+                Console.Out.WriteLine(e.Message);
+            }
+
+        }
     }
 }
