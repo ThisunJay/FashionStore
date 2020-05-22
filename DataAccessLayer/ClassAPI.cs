6660a7a2f68061a98bf1c51beb4c506808eecf38
ThisunJay <jayamaduthisun@gmail.com>
2020-05-22 23:36:17 +0530

    Rest API call class created https://stackoverflow.com/questions/9620278/how-do-i-make-calls-to-a-rest-api-using-c

---
+++ DataAccessLayer/ClassAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ClassAPI
+    {
+        private const string URL = "https://jsonplaceholder.typicode.com/posts";
+        private string urlParameters = "?api_key=123";
+
+    }
+}
---
+++ DataAccessLayer/Posts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Posts
+    {
+        public string PostsObject { get; set; }
+    }
+}
